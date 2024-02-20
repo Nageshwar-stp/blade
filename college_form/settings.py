1fb2e34da5cc5a93f3cad021be5784273f92a402
Nageshwar Tripathi <stpandstp.martialstylus@gmail.com>
2024-02-20 10:57:37 +0530

    added : validation

--- college_form/settings.py
+++ college_form/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!6%1g%zfznqpha-4-tb!(nlma5f&jb^jsaa56k9b5=mlk^z@02'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
